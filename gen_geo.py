9d4aaff8d7539ec567ba5502fc555ba79d73f5da
shibacow <shibacow@gmail.com>
2017-02-10 06:06:36 +0900

    ch data

--- gen_geo.py
+++ gen_geo.py
@@ -16,28 +16,23 @@ def gen():
         c=np.radians(d)
         y=math.sin(c)
         y=y*100
-        y=y+random.normalvariate(0,10)
-        #y=y*random.randint(0,120)
+        y=y+random.normalvariate(0,150)
         y=abs(y)
-        if i>0:
-            prex=xs[i-1]
-            prey=ys[i-1]
-            print("i={} prex={} prey={}".format(i,prex,prey))
-            xs.append(30)
-            ys.append(y-prey)
-        else:
-            xs.append(30)
-            ys.append(y)
+        ys.append(y)
+        xs.append(x*30)
+        ys.append(y)
+        xs.append((x*30)+30)
 
-        #msg="x={} y={}".format(x,y)
+        #sg="x={} y={}".format(x,y)
         #print(msg)
-    #@df2=pd.DataFrame({'Y':ys},index=xs)
+    #df2=pd.DataFrame({'Y':ys},index=xs)
     #plt.plot(df2)
     #plt.show()
+
     return (xs,ys)
 def gen_json(xs,ys):
     t=[dict(x=x,y=y) for (x,y) in zip(xs,ys)]
-    outf=open('out2.json','wb')
+    outf=open('out3.json','wb')
     s=json.dumps(t)
     outf.write(s.encode('utf-8'))
     #print(s)
--- geometory.py
+++ geometory.py
@@ -3,12 +3,29 @@
 import sys,random
 import pygame
 from pygame.locals import *
+from pygame.color import *
 import pymunk
 import pymunk.pygame_util
 import math
 import re
 from pymunk import vec2d
 import json
+point=None
+def draw_collision(arbiter, space, data):
+    #print("hoge")
+    global point
+    for c in arbiter.contact_point_set.points:
+        r = max( 3, abs(c.distance*5) )
+        r = int(r)
+        p = tuple(map(int, c.point_a))
+        point=p
+
+    is_segment=any([c for  c in arbiter.shapes if isinstance(c,pymunk.shapes.Segment)])
+    if not is_segment:
+        point=None
+
+        #print("p={}".format(p))
+        #pygame.draw.circle(data["surface"], THECOLORS["red"], p, r, 0)
 
 def init():
     pygame.init()
@@ -24,32 +41,40 @@ def init():
 
 def up_position(body,dt):
     print("body={} dt={}".format(body,dt))
-def get_resource(space):
-    o=json.load(open('out.json'))
+def get_resource(space,surface):
+    o=json.load(open('out3.json'))
     maxx=10
     radius=14
     body = pymunk.Body(100000,100000)
-    body.position=(300,230)
+    body.position=(300,-1*surface.get_height())
+    body.filter=pymunk.ShapeFilter(categories=2)
     for i,k in enumerate(o[:-1]):
         p1=k
         p2=o[i+1]
         x1=float(p1['x'])
-        y1=float(p1['y'])
+        y1=-1*float(p1['y'])
         x2=float(p2['x'])
-        y2=float(p2['y'])
+        y2=-1*float(p2['y'])
+        (x1,y1)=pymunk.pygame_util.from_pygame((x1,y1),surface)
+        (x2,y2)=pymunk.pygame_util.from_pygame((x2,y2),surface)
+
         l=pymunk.Segment(body,(x1,y1),(x2,y2),1)
+        l.filter=pymunk.ShapeFilter(categories=2)
         space.add(l)
         print("i={} k={}".format(i,k))
     return body
 
 def add_ball(space,x):
+    global debault_color
     mass=10
     radius=14
     inetia = pymunk.moment_for_circle(mass,0,radius,(0,0))
     body = pymunk.Body(mass,inetia)
     #x = random.randint(120,380)
     body.position = x,350
+    body.filter=pymunk.ShapeFilter(categories=1)
     #body.torque = 5000
+    body.velocity=(0.0,-1)
     shape = pymunk.Circle(body,radius,(0,0))
     space.add(body,shape)
     return shape
@@ -80,7 +105,7 @@ def add_joint(space,a,b):
     space.add(j3)
 
 class AnimateResize(object):
-    def __init__(self,outr_init,inner_init,sz):
+    def __init__(self,outr_init,inner_init,sz,space,mouse_pos):
         self.initx=outr_init[0]
         self.inity=outr_init[1]
         self.innerx=inner_init[0]
@@ -91,6 +116,15 @@ class AnimateResize(object):
         self.velocity_y=(self.sy-self.inity)/10
         self.scale=[1,2,3,4]
         self.is_finish=False
+        diffx=(self.sx - self.innerx) / 2
+        diffy=(self.sy - self.innery) /2
+        self.diffp = (diffx,diffy)
+        bd = space.point_query_nearest(mouse_pos, pymunk.inf, pymunk.ShapeFilter(mask=1))
+        if bd:
+            #self.default_color=bd.shape.color
+            bd.shape.color=THECOLORS['pink']
+            self.bd=bd
+
     def update(self):
         d=self.scale.pop()
         self.sx=self.sx - d*self.velocity_x
@@ -100,6 +134,9 @@ class AnimateResize(object):
         draw_options = pymunk.pygame_util.DrawOptions(screen2)
         if not self.scale:
             self.is_finish=True
+            self.bd.shape.color=pymunk.space_debug_draw_options.SpaceDebugColor(r=52, g=152, b=219, a=255)
+            p=self.bd.shape.body.position
+            self.bd.shape.body.position=(p.x + self.diffp[0],p.y+self.diffp[1])
         return (screen,screen2,draw_options)
     def start(self,event):
         screen = pygame.display.set_mode((event.w, event.h),RESIZABLE)
@@ -108,6 +145,7 @@ class AnimateResize(object):
         return (screen,screen2,draw_options)
 
 def main():
+    global point
     (screen,screen2,clock,space) = init()
     draw_options = pymunk.pygame_util.DrawOptions(screen2)
     add_ball(space,400)
@@ -126,10 +164,13 @@ def main():
     running=True
     is_interactive = False
     constraints = space.constraints
-    back_recouce=get_resource(space)
+    back_recouce=get_resource(space,screen2)
     camera_x=0
     verocity=3
     ar=None
+    ch = space.add_collision_handler(0, 0)
+    ch.data["surface"] = screen2
+    ch.post_solve = draw_collision
     while running:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -142,8 +183,12 @@ def main():
             elif event.type == MOUSEBUTTONUP:
                 is_interactive = False
             elif event.type == VIDEORESIZE:
-                ar=AnimateResize((610,610),(600,600),(event.w,event.h))
-                (screen,screen2,draw_options)=ar.start(event)
+                if not ar:
+                    mouse_pos = pymunk.pygame_util.get_mouse_pos(screen)
+                    ar=AnimateResize((610,610),(600,600),(event.w,event.h),space,mouse_pos)
+
+                    #print(mouse_pos)
+                    (screen,screen2,draw_options)=ar.start(event)
         if is_interactive:
             mouse_pos = pymunk.pygame_util.get_mouse_pos(screen)
             bd = space.point_query_nearest(mouse_pos, 10, pymunk.ShapeFilter())
@@ -165,8 +210,19 @@ def main():
             p1=(s.b[0]-verocity,s.b[1])
             s.unsafe_set_endpoints(p0,p1)
         space.debug_draw(draw_options)
+        if point:
+            pt=pymunk.pygame_util.to_pygame(point,screen2)
+            pygame.draw.circle(screen2, THECOLORS["red"], pt, 20, 0)
+            point = None
+        #p2=(600,600)
+        #if p2:
+        #    pt=pymunk.pygame_util.to_pygame(p2,screen2)
+        #    pygame.draw.circle(screen2, THECOLORS["green"], pt, 20, 0)
+        #    p2 = None
+
         pygame.display.flip()
         clock.tick(50)
+        pygame.display.set_caption("幾何:fps: " + str(clock.get_fps()))
 
 
 if __name__=='__main__':
